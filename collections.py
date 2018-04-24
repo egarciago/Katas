eace7b1f07abb308d62a1867b3598b0235ff00df
egarciago <eliezer.ggz@gmail.com>
2018-04-23 21:51:23 -0400

    collections exercises add

---
+++ collections.py
@@ -0,0 +1,24 @@
+
+class collections:
+
+def main(self):
+
+
+def printLettersofWord(self, word):
+	result = []
+
+def printNumbers(self):
+	for num in First100NumbersPairs:
+		print num
+def First100NumbersPairs(self):
+	numbers = []
+	num = 2
+	count = 0
+	while(count < 100):
+		numbers.append(num)
+		num +=2
+		count +=1
+	return numbers
+
+if __name__ == '__main__':
+	main()
