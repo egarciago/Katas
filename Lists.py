350e6a3e1e512a2e5bd27c7bbc9adfd5a200a94e
egarciago <eliezer.ggz@gmail.com>
2018-04-24 21:41:38 -0400

    collection type list view

---
+++ Lists.py
@@ -0,0 +1,19 @@
+print "List implementation"
+#Lista declarada, vacia
+listX = []
+#Lista declarada, con valores de diferentes tipos
+listY = ['y', 'n', 1, 0, True, False]
+#Esta lista contiene las dos anteriores
+listZ = listX + listY
+
+listX.append('Hey!')
+listZA = listX + listY
+#Se recorre la lista, se agrega a una variable cada elemento de la lista
+for result in listZA:
+	print result
+
+#Saber el tamano de la lista
+print 'ListX ' + str(len(listX))
+print 'ListY ' + str(len(listY))
+print 'ListZ ' + str(len(listZ))
+print 'ListZA ' + str(len(listZA))
