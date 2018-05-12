343900a19c30ce25c66a46b6eb9fc28d14d04a0d
egarciago <eliezer.ggz@gmail.com>
2018-05-11 23:04:07 -0400

    set use examples

--- sets.py
+++ sets.py
@@ -25,7 +25,7 @@ print set3.symmetric_defference(set2)
 print set1.difference(set2)
 
 print set1.intersection(set2)
-
+print set2.intersection(set3)
 print set1.union(set2)
 
 print set3.union(set2).union(set1)
